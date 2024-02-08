0b9ac1a69de479f801c0946127226dc8dc8208ce
Abdelrahman-Abdou <abdelrahmanabdou52@gmail.com>
2024-02-08 11:21:32 +0200

    add app css files and error page

---
+++ app/error.js
@@ -0,0 +1,11 @@
+'use client';
+
+const Error = ({ error }) => {
+  return (
+    <div className="error">
+      <h1>An Error occured</h1>
+      <p>Something wen wrong</p>
+    </div>
+  );
+};
+export default Error;
